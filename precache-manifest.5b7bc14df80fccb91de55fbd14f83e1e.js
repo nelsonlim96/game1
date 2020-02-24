5092ce0853ad2c90cb71b8fe7e190c5ecfdcb183
Nelson Lim <30296067+nelsonlim96@users.noreply.github.com>
2020-02-24 22:36:39 +0800

    Updates

--- precache-manifest.5b7bc14df80fccb91de55fbd14f83e1e.js
+++ precache-manifest.5b7bc14df80fccb91de55fbd14f83e1e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "743269dda8580d9444511c408b58ef55",
+    "revision": "ba03e3dedb72b5be053cf648b4a29b26",
     "url": "/game1/index.html"
   },
   {
-    "revision": "bb73e4acd5da36491de7",
-    "url": "/game1/static/css/main.7e2f7a94.chunk.css"
+    "revision": "2694cff146524f9fb3a4",
+    "url": "/game1/static/css/main.6de8614b.chunk.css"
   },
   {
     "revision": "6b9c3fe6f559bba2cb53",
     "url": "/game1/static/js/2.bebc9070.chunk.js"
   },
   {
-    "revision": "bb73e4acd5da36491de7",
-    "url": "/game1/static/js/main.7da5b47a.chunk.js"
+    "revision": "2694cff146524f9fb3a4",
+    "url": "/game1/static/js/main.ae4c5f4b.chunk.js"
   },
   {
     "revision": "bc6ae195855f00790b06",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/game1/precache-manifest.af197c6e90a16522555c8104d8700e37.js"
+  "/game1/precache-manifest.5b7bc14df80fccb91de55fbd14f83e1e.js"
 );
 
 self.addEventListener('message', (event) => {
